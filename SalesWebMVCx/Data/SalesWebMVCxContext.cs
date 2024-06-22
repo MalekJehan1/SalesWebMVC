a4b9b2670f84d85cb749c0c12e7e322621a889d6
Malek Jehani <malek.r.j@hotmail.com>
2024-06-22 14:14:03 -0300

    Other entities and second migration

--- SalesWebMVCx/Data/SalesWebMVCxContext.cs
+++ SalesWebMVCx/Data/SalesWebMVCxContext.cs
@@ -14,6 +14,9 @@ namespace SalesWebMVCx.Data
         {
         }
 
-        public DbSet<SalesWebMVCx.Models.Departament> Departament { get; set; }
+        public DbSet<Departament> Departament { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
