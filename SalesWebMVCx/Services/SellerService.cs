10f72c190717a8b10b5bedc1f6cb5fde578b44cd
Malek Jehani <malek.r.j@hotmail.com>
2024-06-24 15:59:51 -0300

    Update seller and custom service exception

--- SalesWebMVCx/Services/SellerService.cs
+++ SalesWebMVCx/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMVCx.Services.Exceptions;
 
 namespace SalesWebMVCx.Services
 {
@@ -38,6 +39,23 @@ namespace SalesWebMVCx.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
